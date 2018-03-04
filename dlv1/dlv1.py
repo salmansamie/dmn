c89e5c358a5295a7158acba80b666f3f6c06d957
Salman Rahman <salman.rahman@se14.qmul.ac.uk>
2018-03-04 15:24:12 +0000

    following in progress

--- dlv1/dlv1.py
+++ dlv1/dlv1.py
@@ -2,9 +2,11 @@ import os
 import configparser
 import tweepy
 import json
+import time
 
 
 __author__ = "salmansamie"
+
 
 # Parse configuration file and get keys, tokens and secrets
 config = configparser.ConfigParser()
@@ -22,11 +24,66 @@ api = tweepy.API(auth)
 
 
 # Collecting Data : Profile
-ep1_user = api.me()._json                                   # alt: api.get_user('samie21s')
-with open('./output/Profile.json', 'w') as fp1:
-    json.dump(ep1_user, fp1, sort_keys=True, indent=4)
-
-print(ep1_user)
+def get_profile():
+    ep1_user = api.me()._json                                   # alt: api.get_user('samie21s')
+    with open('./output/Profile.json', 'w') as fp1:
+        json.dump(ep1_user, fp1, sort_keys=True, indent=4)
+    print(ep1_user)
 
 
 # Collecting Data : Timeline
+def get_timeline():
+    ep2_timeline = api.user_timeline('samie21s')[0]._json
+    with open('./output/Timeline.json', 'w') as fp2:
+        json.dump(ep2_timeline, fp2, sort_keys=True, indent=4)
+    print(ep2_timeline)
+
+
+# Collecting Data : Followers
+def get_followers():
+    ep3_followers = []
+    for user in tweepy.Cursor(api.followers, screen_name="samie21s").items():
+        ep3_followers.append({"id": user.id,
+                              "id_str": user.id_str,
+                              "name": user.name,
+                              "screen_name": user.screen_name,
+                              "location": user.location,
+                              "description": user.description,
+                              "url": user.url,
+                              "entities": user.entities,
+                              "protected": user.protected,
+                              "followers_count": user.followers_count,
+                              "friends_count": user.friends_count,
+                              "listed_count": user.listed_count,
+                              "created_at": user.created_at,
+                              "favourites_count": user.favourites_count,
+                              "geo_enabled": user.geo_enabled,
+                              "lang": user.lang,
+                              "profile_image_url": user.profile_image_url,
+                              "profile_background_image_url": user.profile_background_image_url,
+                              "profile_image_url_https": user.profile_image_url_https,
+                              "default_profile": user.default_profile,
+                              "follow_request_sent": user.follow_request_sent,
+                              "notifications": user.notifications,
+                              "muting": user.muting,
+                              "blocking": user.blocking,
+                              "blocked_by": user.blocked_by
+                              })
+
+    with open('./output/Followers.json', 'w') as fp3:
+        fp3.write(str(ep3_followers))
+    print(ep3_followers)
+
+
+# Collecting Data : Friends/Following
+def get_friends():
+    for friend in tweepy.Cursor(api.friends('samie21s')).items():
+        # Process the friend here
+        print(friend)
+
+
+if __name__ == '__main__':
+    # get_profile()
+    # get_timeline()
+    # get_followers()
+    get_profile()
